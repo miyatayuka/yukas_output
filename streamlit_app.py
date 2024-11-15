fb69aa46bb8fcf7945613e114a6d45e3d8021f8d
miyatayuka <miyata.yuka.4923@members.co.jp>
2024-11-15 16:00:44 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
-
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 # Write directly to the app
 st.title("データ分析アプリ:bar_chart:")
